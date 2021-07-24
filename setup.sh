44f1608c2c19d1b467e60b64e4290512aa6e66a6
Sean <snaramor@live.com>
2021-07-23 23:53:39 -0700

    Update setup.sh
    missing line break

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
-mkdir -p ~/.streamlit/echo “\
+mkdir -p ~/.streamlit/
+echo “\
 [general]\n\
 email = \”snaramor@live.com\”\n\
-“ > ~/.streamlit/credentials.tomlecho “\
+“ > ~/.streamlit/credentials.toml
+echo “\
+
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 “ > ~/.streamlit/config.toml
+
